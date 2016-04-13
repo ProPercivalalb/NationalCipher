9942030dcec018f987462c4519fa229c4ce7b2be
ProPercivalalb <propercivalalb@gmail.com>
2016-04-13 08:00:08 +0100

    Decodeder

--- src/nationalcipher/cipher/PeriodicGromark.java
+++ src/nationalcipher/cipher/PeriodicGromark.java
@@ -1,11 +1,22 @@
 package nationalcipher.cipher;
 
+import javalibrary.lib.Timer;
+import javalibrary.math.MathHelper;
 import javalibrary.string.StringTransformer;
 
 public class PeriodicGromark {
 
 	public static void main(String... args) {
-		System.out.println(encode("PHASEFIVESEAHORSEISREADYFORTRIALSANDTHENAUTILUSSYSTEMISFULLYFUNCTIONALWEENGAGEDTHEMECHANISMANDLOWEREDTHEDECKTOTHREEFEETABOVESEALEVELAPPROACHINGTHESHOREBYTHERADARSTATIONATALLTIMESSIGNALSFROMTHEIRCOMMUNICATIONSWEREMONITOREDANDNOSIGNWASGIVENTHATOURAPPROACHHADBEENMONITOREDOREVENNOTICEDWEBACKEDOFFTHEDECKWASRAISEDBYTWOFEETANDTHEAPPROACHATTEMPTEDAGAINONCEMOREOURINCURSIONWASUNNOTICEDOVERNIGHTWECONDUCTEDARANGEOFTESTSANDMAPPEDTHERADARCOVERAGEONTHREESEPARATEOCCASIONSTHERESEEMSTOHAVEBEENAFLURRYOFACTIVITYANDOURMODELINGSUGGESTSTHATTHESHIPSMASTSMAYHAVETRIGGEREDBRIEFALARMSONALLOCCASIONSTHEAUTOMATICDIVESYSTEMSCUTINCORRECTLYLOWERINGTHEDECKSTOSEALEVELANDTHEALARMSWERECANCELLEDTHESEAHORSEDEPLOYMENTSYSTEMWILLBEFULLYMOUNTEDTONIGHTANDWEWILLCONDUCTABATTERYOFTESTSONTHEDEPLOYMENTANDEMERGENCYRECOVERYSYSTEMSOVERTHENEXTTWONIGHTSASSUMINGTHATSEAANDAIRTRAFFICREMAINSLOW", "ARELYONGK"));
+		Timer timer = new Timer();
+		String text = "PHASEFIVESEAHORSEISREADYFORTRIALSANDTHENAUTILUSSYSTEMISFULLYFUNCTIONALWEENGAGEDTHEMECHANISMANDLOWEREDTHEDECKTOTHREEFEETABOVESEALEVELAPPROACHINGTHESHOREBYTHERADARSTATIONATALLTIMESSIGNALSFROMTHEIRCOMMUNICATIONSWEREMONITOREDANDNOSIGNWASGIVENTHATOURAPPROACHHADBEENMONITOREDOREVENNOTICEDWEBACKEDOFFTHEDECKWASRAISEDBYTWOFEETANDTHEAPPROACHATTEMPTEDAGAINONCEMOREOURINCURSIONWASUNNOTICEDOVERNIGHTWECONDUCTEDARANGEOFTESTSANDMAPPEDTHERADARCOVERAGEONTHREESEPARATEOCCASIONSTHERESEEMSTOHAVEBEENAFLURRYOFACTIVITYANDOURMODELINGSUGGESTSTHATTHESHIPSMASTSMAYHAVETRIGGEREDBRIEFALARMSONALLOCCASIONSTHEAUTOMATICDIVESYSTEMSCUTINCORRECTLYLOWERINGTHEDECKSTOSEALEVELANDTHEALARMSWERECANCELLEDTHESEAHORSEDEPLOYMENTSYSTEMWILLBEFULLYMOUNTEDTONIGHTANDWEWILLCONDUCTABATTERYOFTESTSONTHEDEPLOYMENTANDEMERGENCYRECOVERYSYSTEMSOVERTHENEXTTWONIGHTSASSUMINGTHATSEAANDAIRTRAFFICREMAINSLOW";
+		String key = "WELCOM";
+		
+		String encoded = encode(text, key);
+		
+		System.out.println(encoded);
+		String decoded = new String(decode(encoded.toCharArray(), key));
+		System.out.println(decoded);
+		timer.displayTime();
 	}
 	
 	public static String encode(String plainText, String key) {
@@ -19,11 +30,10 @@ public class PeriodicGromark {
 				inOrd[p++] = keyindex;
 				noOrd[keyindex] = p;
 			}
+			else
+				key += ch;
 		}
 		
-		for(char ch = 'A'; ch <= 'Z'; ++ch)
-			if(!key.contains("" + ch))
-				key += ch;
 		
 		String transposedKey = "";
 		int[] numericKey = new int[plainText.length()];
@@ -44,5 +54,43 @@ public class PeriodicGromark {
 		}
 		
 		return cipherText;
+	}
+	
+	public static char[] decode(char[] cipherText, String key) {
+		int[] inOrd = new int[key.length()];
+		int[] noOrd = new int[key.length()];
+		
+		int p = 0;
+		for(char ch = 'A'; ch <= 'Z'; ++ch) {
+			int keyindex = key.indexOf(ch);
+			if(keyindex != -1) {
+				inOrd[p++] = keyindex;
+				noOrd[keyindex] = p;
+			}
+			else
+				key += ch;
+		}
+		
+		
+		String transposedKey = "";
+		int[] numericKey = new int[cipherText.length];
+		
+		for(int i = 0; i < inOrd.length; i++) {
+			transposedKey += StringTransformer.getEveryNthChar(key, inOrd[i], inOrd.length);
+			numericKey[i] = noOrd[i];
+		}
+
+		for(int i = 0; i < numericKey.length - noOrd.length; i++)
+			numericKey[i + noOrd.length] = (numericKey[i] + numericKey[i + 1]) % 10;
+		
+		
+		char[] plainText = new char[cipherText.length];
+		
+		for(int i = 0; i < cipherText.length; i++) {
+			int keyIndex = (int)(Math.floor(i / inOrd.length) % inOrd.length);
+			plainText[i] = (char)(MathHelper.mod(transposedKey.indexOf(cipherText[i]) - transposedKey.indexOf(key.charAt(keyIndex)) - numericKey[i], 26) + 'A');
+		}
+		
+		return plainText;
 	}
 }
