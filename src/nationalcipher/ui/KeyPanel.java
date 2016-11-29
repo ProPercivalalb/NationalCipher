17e4219c63a80a0fa0fe362426c179f04d7c420f
ProPercivalalb <propercivalalb@gmail.com>
2016-11-29 08:03:58 +0000

    Invisible boxes instead of spaces

--- src/nationalcipher/ui/KeyPanel.java
+++ src/nationalcipher/ui/KeyPanel.java
@@ -2,6 +2,7 @@ package nationalcipher.ui;
 
 import java.awt.Dimension;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -29,26 +30,31 @@ public class KeyPanel extends JPanel {
 		this.fitness.setMaximumSize(new Dimension(150, Integer.MAX_VALUE));
 		this.key.setMaximumSize(new Dimension(290, Integer.MAX_VALUE));
 		this.iterations.setMaximumSize(new Dimension(100, Integer.MAX_VALUE));
-		this.add(new JLabel("Fitness "));
+		this.add(new JLabel("Fitness"));
+		this.add(Box.createRigidArea(new Dimension(5, 0)));
 		this.add(this.fitness);
-		this.add(new JLabel(" Key "));
+		this.add(Box.createRigidArea(new Dimension(5, 0)));
+		this.add(new JLabel("Key"));
+		this.add(Box.createRigidArea(new Dimension(5, 0)));
 		this.add(this.key);
-		this.add(new JLabel(" Iteration "));
+		this.add(Box.createRigidArea(new Dimension(5, 0)));
+		this.add(new JLabel("Iteration"));
+		this.add(Box.createRigidArea(new Dimension(5, 0)));
 		this.add(this.iterations);
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		this.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
 	}
 	
 	public void updateIteration(int n) {
-		if(settings.updateProgress())
+		if(this.settings.updateProgress())
 			this.iterations.setText(String.valueOf(n));
 	}
 	
 	public void setIterationUnsed() {
-		if(!settings.updateProgress())
-			this.iterations.setText("-------------");
+		if(!this.settings.updateProgress())
+			this.iterations.setEnabled(false);
 		else
-			this.iterations.setText("");
+			this.iterations.setEnabled(true);
 	}
 
 	public void updateSolution(Solution solution) {
